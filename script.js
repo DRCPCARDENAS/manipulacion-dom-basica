456e2efceee152e485bbf12533101f9bcc4406ca
Roberto Cardenas <drcp_cardenas@hotmail.com>
2023-03-21 21:23:52 -0600

    Agregar .gitignore y primeros archivos del proyecto

---
+++ script.js
@@ -0,0 +1,48 @@
+
+const h1=document.querySelector('h1');
+const form=document.getElementById('form');
+const input1=document.querySelector('#calculo1');
+const input2=document.querySelector('#calculo2');
+const btn= document.querySelector('#btnCalcular');
+const pResult =document.querySelector('#result')
+
+// form.addEventListener  ('submit',sumarInputFalso);
+
+// function sumarInputFalso(event){
+//     event.preventDefault();
+//     const sumaInputs= input1.value + input2.value;
+//     pResult.innerText= 'Resultado '+ sumaInputs;
+// }
+
+btn.addEventListener
+ 
+
+
+// const p=document.querySelector('p');
+// const parrafito=document.querySelector('.parrafito ');
+// const pid=document.querySelector('#pid');
+
+
+// console.log(input.value);
+// console.log({
+//     h1,
+//     p,
+//     parrafito,
+//     pid,
+//     input
+// });
+// h1.innerHTML='Patito <br> Feo';
+// h1.innerText='Patito <br> Feo';
+// console.log(h1.getAttribute('class'));
+// h1.setAttribute('class','rojo');
+// h1.classList.add('rojo');
+// h1.classList.remove('verde');
+// h1.classList.toggle('verde');
+// h1.classList.contains('verde'); //Nos devuelve true o false si la clase encuentra en unelemento
+// input.value="456";
+// const img=document.createElement('img');
+// img.setAttribute('src','https://images.pexels.com/photos/15332223/pexels-photo-15332223.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1');
+// pid.innerHTML='';
+// pid.appendChild(img);
+// pid.innerText(img);
+
